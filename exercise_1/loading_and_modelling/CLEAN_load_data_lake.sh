4e05e9b34aaa46c78851be8b523202cc24f2300b
w205 <w205@ip-172-31-1-147.ec2.internal>
2017-02-19 22:54:23 +0000

    clean functions second version

--- exercise_1/loading_and_modelling/CLEAN_load_data_lake.sh
+++ exercise_1/loading_and_modelling/CLEAN_load_data_lake.sh
@@ -9,14 +9,20 @@ rmdir ~/staging/exercise_1
 rmdir ~/staging
 
 #remove files from hdfs
-hdfs dfs -rm /user/w205/hospital_compare/hospitals.csv
-hdfs dfs -rm /user/w205/hospital_compare/effective_care.csv
-hdfs dfs -rm /user/w205/hospital_compare/surveys_responses.csv
-hdfs dfs -rm /user/w205/hospital_compare/Measures.csv
-hdfs dfs -rm /user/w205/hospital_compare/readmissions.csv
+hdfs dfs -rm /user/w205/hospital_compare/hospitals/hospitals.csv
+hdfs dfs -rm /user/w205/hospital_compare/effective/effective_care.csv
+hdfs dfs -rm /user/w205/hospital_compare/surveys/surveys_responses.csv
+hdfs dfs -rm /user/w205/hospital_compare/measures/Measures.csv
+hdfs dfs -rm /user/w205/hospital_compare/readmissions/readmissions.csv
 
 # removeour hdfs directory
+hdfs dfs -rmdir /user/w205/hospital_compare/hospitals
+hdfs dfs -rmdir /user/w205/hospital_compare/effective
+hdfs dfs -rmdir /user/w205/hospital_compare/surveys
+hdfs dfs -rmdir /user/w205/hospital_compare/measures
+hdfs dfs -rmdir /user/w205/hospital_compare/readmissions
 hdfs dfs -rmdir /user/w205/hospital_compare
+
 
 #change directory back to original
 cd $MY_CWD
